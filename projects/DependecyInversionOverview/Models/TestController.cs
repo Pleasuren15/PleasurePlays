ea1426eb89864144df19b95c4b5a52dc40d210ba
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-06-29 17:59:22 +0200

    wip: changes implemented

---
+++ projects/DependecyInversionOverview/Models/TestController.cs
@@ -0,0 +1,32 @@
+﻿using DependecyInversionOverview.Models.Interfaces;
+
+namespace DependecyInversionOverview.Models
+{
+    internal class TestController(ISingletonService singletonService, IScopedService scopedService, ITransientService transientService)
+    {
+        private readonly ISingletonService _singletonService = singletonService;
+        private readonly IScopedService _scopedService = scopedService;
+        private readonly ITransientService _transientService = transientService;
+
+        public void TestSingletonService()
+        {
+            Console.WriteLine("Name: {0}", _singletonService.Name);
+            //Console.WriteLine("Description: {0}", _singletonService.Description);
+            Console.WriteLine("InstanceId: {0}", _singletonService.InstanceId);
+        }
+
+        public void TestTransientService()
+        {
+            Console.WriteLine("Name: {0}", _transientService.Name);
+            //Console.WriteLine("Description: {0}", _transientService.Description);
+            Console.WriteLine("InstanceId: {0}", _transientService.InstanceId);
+        }
+
+        public void TestScopedService()
+        {
+            Console.WriteLine("Name: {0}", _scopedService.Name);
+            //Console.WriteLine("Description: {0}", _scopedService.Description);
+            Console.WriteLine("InstanceId: {0}", _scopedService.InstanceId);
+        }
+    }
+}
--- projects/DependecyInversionOverview/Program.cs
+++ projects/DependecyInversionOverview/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using Cocona;
+using DependecyInversionOverview.Models;
+using DependecyInversionOverview.Models.Implementation;
+using DependecyInversionOverview.Models.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+
+var builder = CoconaApp.CreateBuilder();
+builder.Logging.AddFilter("System.Net.Http.HttpClient", LogLevel.Warning);
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<ISingletonService, SingletonService>();
+builder.Services.AddTransient<ITransientService, TransientService>();
+builder.Services.AddSingleton<IScopedService, ScopedService>();
+
+var app = builder.Build();
+
+app.AddCommand("singletonServiceOverview", () =>
+{
+    Console.WriteLine("Test Singleton LifeTime");
+    for (int i = 0; i < 3; i++)
+    {
+        var testController = new TestController(
+            app.Services.GetRequiredService<ISingletonService>(),
+            app.Services.GetRequiredService<IScopedService>(),
+            app.Services.GetRequiredService<ITransientService>());
+
+        testController.TestSingletonService();
+    }
+
+    Console.WriteLine("\n\nTest Scoped LifeTime");
+    for (int i = 0; i < 3; i++)
+    {
+        var testController = new TestController(
+            app.Services.GetRequiredService<ISingletonService>(),
+            app.Services.GetRequiredService<IScopedService>(),
+            app.Services.GetRequiredService<ITransientService>());
+
+        testController.TestScopedService();
+    }
+
+    Console.WriteLine("\n\nTest Transient LifeTime");
+    for (int i = 0; i < 3; i++)
+    {
+        var testController = new TestController(
+            app.Services.GetRequiredService<ISingletonService>(),
+            app.Services.GetRequiredService<IScopedService>(),
+            app.Services.GetRequiredService<ITransientService>());
+
+        testController.TestTransientService();
+    }
+});
+
+
+app.Run();
