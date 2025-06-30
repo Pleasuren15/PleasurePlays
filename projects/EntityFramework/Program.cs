971616a8bf84daaa77164c3740649191c78e3906
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-06-30 20:57:15 +0200

    wip: builder services added

--- projects/EntityFramework/Program.cs
+++ projects/EntityFramework/Program.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
+﻿using EntityFramework.Infrastructure;
 
 internal class Program
 {
     private static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+        builder.Services.AddSqlServer<AppDbContext>(builder.Configuration["ConnectionStrings:PleasurePlaysDatabase"]);
 
         var app = builder.Build();
         await app.RunAsync();
